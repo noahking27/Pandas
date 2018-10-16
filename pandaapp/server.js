bae06bd47ddc18ddb98890e383a9f900d47ae68e
Noah King <noahking@Noahs-MacBook-Pro.local>
2018-10-16 19:02:09 -0400

    add html and api routes

--- pandaapp/server.js
+++ pandaapp/server.js
@@ -4,12 +4,9 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 app.listen(process.env.PORT || 8080);
+
+console.log("App listening on PORT: 8080");
