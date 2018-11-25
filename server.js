1d2f51a3f15a06ef4c8243b66aff43f32d5d2568
Noah King <noahking@Noahs-MacBook-Pro.local>
2018-11-25 02:02:32 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(express.static(path.join(__dirname, 'build')));
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+  });
+
 app.listen(process.env.PORT || 8080);
 
 console.log("App listening on PORT: 8080");
