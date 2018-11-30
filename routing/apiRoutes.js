acb77ae51757e410fa30070c60d544d5a18313f3
Noah King <noahking@Noahs-MacBook-Pro.local>
2018-11-29 20:11:51 -0500

    works locally

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,20 +1,20 @@
 const pg = require('pg');
 
-// const pool = new pg.Pool({
-//   user: process.env.AWS_USERNAME,
-//   host: process.env.AWS_HOST,
-//   database: process.env.AWS_DATABASE,
-//   password: process.env.AWS_PWD,
-//   port: process.env.AWS_PORT
-// });
+const pool = new pg.Pool({
+  user: process.env.AWS_USERNAME,
+  host: process.env.AWS_HOST,
+  database: process.env.AWS_DATABASE,
+  password: process.env.AWS_PWD,
+  port: process.env.AWS_PORT
+});
 
 module.exports = function (app) {
   app.get('/states', function (req, res) {
-    // pool.query('Select * from public.States order by States ASC').then(function({ rows }) {
-      // console.log(rows);
-      // res.json(rows);
-      // pool.end();
-    // });
+    pool.query('Select * from public.States order by States ASC').then(function({ rows }) {
+      console.log(rows);
+      res.json(rows);
+      pool.end();
+    });
 
     // pool.query('Select * from public.cities_extended order by States ASC').then(function({ rows }) {
     //   console.log(rows);
@@ -31,11 +31,11 @@ module.exports = function (app) {
     //   pool.end();
     // });
 
-    // pool.query('SELECT city, state_code, zip, county, "ID", "Latitude", "Longitude" FROM public.cities_extended where state_code = "PR"').then(function({ rows }) {
-    //   console.log(rows);
-    //   res.json(rows);
-    //   pool.end();
-    // })
+    pool.query('SELECT city, state_code, zip, county, "ID", "Latitude", "Longitude" FROM public.cities_extended where state_code = "PR"').then(function({ rows }) {
+      console.log(rows);
+      res.json(rows);
+      pool.end();
+    })
 
     // pool.query('Select * from public.cities_extended order by States ASC').then(function({ rows }) {
     //   console.log(rows);
